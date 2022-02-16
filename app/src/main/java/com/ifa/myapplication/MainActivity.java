0c322cf6636a22f44a239f1ff349acea343301a6
afwanaditya <afwanaditya2005@gmail.com>
2022-02-16 10:23:44 +0700

    Initial commit

---
+++ app/src/main/java/com/ifa/myapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.ifa.myapplication;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }public void profile(View view) {
+        Intent intent = new Intent(MainActivity.this, profilActivity.class);
+        startActivity(intent);
+    }
+
+    public void galeri(View view) {
+        Intent intent = new Intent(MainActivity.this, galeryActivity.class);
+        startActivity(intent);
+    }
+
+    public void ekskul(View view) {
+        Intent intent = new Intent(MainActivity.this, eskulActivity.class);
+        startActivity(intent);
+    }
+
+    public void perpus(View view) {
+        Intent intent = new Intent(MainActivity.this, perpusActivity.class);
+        startActivity(intent);
+    }
+
+    public void guru(View view) {
+        Intent intent = new Intent(MainActivity.this, guruActivity.class);
+        startActivity(intent);
+    }
+
+    public void fasilitas(View view) {
+        Intent intent = new Intent(MainActivity.this, fasilitasActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/ifa/myapplication/backupActivity.java
@@ -0,0 +1,14 @@
+package com.ifa.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class backupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backup);
+    }
+}
